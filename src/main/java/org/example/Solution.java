57057a14ab363b90c0bbb1445e7e452561bd3857
alek <wait82985@gmail.com>
2023-05-12 21:51:25 +0600

    problem solved

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,40 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
     public String countAndSay(int n) {
+        String result = "1";
+        while ( n != 1 ){
+           result = createString(stringToCountArray(result));
+           n--;
+        }
+        return result;
+    }
 
+    public List<int[]> stringToCountArray(String number){
+        List<int[]> list = new ArrayList<>();
+        int count = 1;
+        int previousNumber = number.charAt(0) - '0';
+        for (int i = 1; i < number.length(); i++) {
+            if(previousNumber == number.charAt(i) - '0'){
+                count++;
+            }else {
+                list.add(new int[]{count, previousNumber});
+                previousNumber = number.charAt(i) - '0';
+                count = 1;
+            }
+        }
+        list.add(new int[]{count, previousNumber});
+        return list;
+    }
+    public String createString(List<int[]> list){
+        StringBuilder sb = new StringBuilder();
+        for (int[] x: list) {
+            sb.append(x[0]);
+            sb.append(x[1]);
+        }
+        return sb.toString();
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -2,6 +2,8 @@ import org.example.Solution;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class SolutionTest {
     @Test
     public void countAndSayTest1(){
@@ -11,5 +13,11 @@ public class SolutionTest {
     @Test
     public void countAndSayTest2(){
         Assert.assertEquals("1211", new Solution().countAndSay(4));
+    }
+
+    @Test
+    public void helper1Test(){
+//        List<int[]> list = List.of(new int[]{2,2}, new int[]{2,3}, new int[]{1,1}, new int[]{5,4}, new int[]{2,1});
+        Assert.assertEquals("2223115421", new Solution().createString(new Solution().stringToCountArray("223314444411")));
     }
 }
